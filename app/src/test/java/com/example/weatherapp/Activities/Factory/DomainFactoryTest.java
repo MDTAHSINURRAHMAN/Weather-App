c15c1fdef65b90333235f8b073b98a1a62088eb4
cryptic0053 <anirbanargha0053@gmail.com>
2024-05-28 23:48:10 +0600

    unit test working

--- app/src/test/java/com/example/weatherapp/Activities/Factory/DomainFactoryTest.java
+++ app/src/test/java/com/example/weatherapp/Activities/Factory/DomainFactoryTest.java
@@ -1,9 +1,33 @@
 package com.example.weatherapp.Activities.Factory;
 
-import junit.framework.TestCase;
+import com.example.weatherapp.Activities.Domains.Hourly;
+import com.example.weatherapp.Activities.Domains.TommorowDomain;
+import com.example.weatherapp.Activities.Factory.DomainFactory;
 
-public class DomainFactoryTest extends TestCase {
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-    public void testCreateDomain() {
+public class DomainFactoryTest {
+
+    @Test
+    public void testCreateHourlyDomain() {
+        Object domain = DomainFactory.createDomain("Hourly", "12:00", 25.5, "path/to/pic.jpg");
+        assertTrue(domain instanceof Hourly);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidDomainType() {
+        DomainFactory.createDomain("InvalidType", "args");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidHourlyArguments() {
+        DomainFactory.createDomain("Hourly", "12:00", "invalidTemp", "path/to/pic.jpg");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidTomorrowArguments() {
+        DomainFactory.createDomain("Tomorrow", "Monday", "path/to/pic.jpg", "Sunny", "invalidHighTemp", "invalidLowTemp");
     }
 }
