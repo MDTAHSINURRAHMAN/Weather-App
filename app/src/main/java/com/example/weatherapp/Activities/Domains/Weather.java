e80eceb2e335a64b9dc1910891de05fe9834efc5
WOWAbid <hasan2007107@stud.kuet.ac.bd>
2024-05-29 13:40:02 +0600

    docs for weather added

--- app/src/main/java/com/example/weatherapp/Activities/Domains/Weather.java
+++ app/src/main/java/com/example/weatherapp/Activities/Domains/Weather.java
@@ -1,5 +1,14 @@
 package com.example.weatherapp.Activities.Domains;
 
+/**
+ * An interface representing weather data.
+ */
 public interface Weather {
+
+    /**
+     * Gets the path to the weather icon image.
+     *
+     * @return the path to the weather icon image
+     */
     String getPicPath();
 }
