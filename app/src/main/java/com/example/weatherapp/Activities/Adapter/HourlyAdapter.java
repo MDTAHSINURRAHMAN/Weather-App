b0175a0fd61d26904b9e7b0667eed0a057c2cac1
WOWAbid <hasan2007107@stud.kuet.ac.bd>
2024-05-29 13:32:58 +0600

    docs for hourly adapter added

--- app/src/main/java/com/example/weatherapp/Activities/Adapter/HourlyAdapter.java
+++ app/src/main/java/com/example/weatherapp/Activities/Adapter/HourlyAdapter.java
@@ -17,15 +17,31 @@ import com.example.weatherapp.R;
 
 import java.util.ArrayList;
 
+/**
+ * Adapter class for displaying hourly weather data in a RecyclerView.
+ */
 public class HourlyAdapter extends RecyclerView.Adapter<HourlyAdapter.ViewHolder> {
     private ArrayList<Hourly> items;
     private Context context;
 
+    /**
+     * Constructor for HourlyAdapter.
+     *
+     * @param context the context for resource access and view inflation
+     * @param items   the list of hourly weather data
+     */
     public HourlyAdapter(Context context, ArrayList<Hourly> items) {
         this.context = context;
         this.items = items;
     }
 
+    /**
+     * Creates a new ViewHolder for the RecyclerView.
+     *
+     * @param parent   the parent ViewGroup into which the new view will be added
+     * @param viewType the view type of the new view
+     * @return a new ViewHolder instance
+     */
     @NonNull
     @Override
     public HourlyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -33,6 +49,12 @@ public class HourlyAdapter extends RecyclerView.Adapter<HourlyAdapter.ViewHolder
         return new ViewHolder(inflate);
     }
 
+    /**
+     * Binds the data to the views in the ViewHolder for the specified position.
+     *
+     * @param holder   the ViewHolder that should be updated
+     * @param position the position of the item within the adapter's data set
+     */
     @Override
     public void onBindViewHolder(@NonNull HourlyAdapter.ViewHolder holder, int position) {
         Hourly hourly = items.get(position);
@@ -49,15 +71,28 @@ public class HourlyAdapter extends RecyclerView.Adapter<HourlyAdapter.ViewHolder
         }
     }
 
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return the total number of items
+     */
     @Override
     public int getItemCount() {
         return items != null ? items.size() : 0;
     }
 
+    /**
+     * ViewHolder class for holding the views for each hourly weather item.
+     */
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView hourTxt, tempTxt;
         ImageView pic;
 
+        /**
+         * Constructor for ViewHolder.
+         *
+         * @param itemView the view of the item
+         */
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             hourTxt = itemView.findViewById(R.id.hourTxt);
